054c59626348d6d6635ae3def8f87416ceb78597
Grant Ammons <gammons@gmail.com>
2023-07-02 07:59:18 -0400

    try ddterm

--- nixbook/configuration.nix
+++ nixbook/configuration.nix
@@ -1,9 +1,6 @@
-# Edit this configuration file to define what should be installed on your system.  Help is available in the configuration.nix(5) man page and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 { imports =
-    [ # Include the results of the hardware scan.
       ./hardware-configuration.nix ];
 
   # Bootloader.
@@ -15,10 +12,8 @@
   boot.initrd.secrets = { "/crypto_keyfile.bin" = null;
   };
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixbook";
   # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary networking.proxy.default = "http://user:password@proxy:port/"; networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -66,6 +61,7 @@
       gnomeExtensions.quake-mode
       gnomeExtensions.openweather
       gnomeExtensions.system-monitor
+      gnomeExtensions.ddterm
 
       obsidian
       spotify
@@ -95,6 +91,7 @@
     neovim
   ];
 
+  # configure nix settings
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   nix.settings.auto-optimise-store = true;
