86d93ae0409185c060d9b94bd8228d035a756ff1
Grant Ammons <gammons@gmail.com>
2023-07-02 07:28:35 -0400

    WIP

---
+++ configuration.nix
@@ -0,0 +1,108 @@
+# Edit this configuration file to define what should be installed on your system.  Help is available in the configuration.nix(5) man page and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{ imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true; boot.loader.efi.canTouchEfiVariables = true;
+
+  # Setup keyfile
+  boot.initrd.secrets = { "/crypto_keyfile.bin" = null;
+  };
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary networking.proxy.default = "http://user:password@proxy:port/"; networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = { LC_ADDRESS = "en_US.UTF-8"; LC_IDENTIFICATION = "en_US.UTF-8"; LC_MEASUREMENT = "en_US.UTF-8"; LC_MONETARY = "en_US.UTF-8"; LC_NAME = "en_US.UTF-8"; LC_NUMERIC = "en_US.UTF-8"; LC_PAPER = 
+    "en_US.UTF-8"; LC_TELEPHONE = "en_US.UTF-8"; LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true; 
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = { layout = "us"; xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true; hardware.pulseaudio.enable = false; security.rtkit.enable = true; services.pipewire = {
+    enable = true; alsa.enable = true; alsa.support32Bit = true; pulse.enable = true;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.grant = { isNormalUser = true; description = "Grant Ammons"; extraGroups = [ "networkmanager" "wheel" ]; packages = with pkgs; [
+      google-chrome
+      gnome.gnome-control-center
+      gnome.gnome-tweaks
+      gnome.gnome-settings-daemon
+      gnomeExtensions.tiling-assistant
+      gnomeExtensions.quake-mode
+      gnomeExtensions.openweather
+      gnomeExtensions.system-monitor
+
+      obsidian
+      spotify
+      kitty
+    ];
+  };
+
+  programs.zsh.enable = true;
+  users.users.grant.shell = pkgs.zsh;
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+    nerdfonts
+  ];
+
+
+  # List packages installed in system profile. To search, run: $ nix search wget
+  environment.systemPackages = with pkgs; [
+    neovim
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nix.settings.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  }
+
+  # This value determines the NixOS release from which the default settings for stateful data, like file locations and database versions on your system were taken. It‘s perfectly fine and recommended to leave this value at the 
+  # release version of the first install of this system. Before changing this value read the documentation for this option (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
--- flake.nix
+++ flake.nix
@@ -10,20 +10,48 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."grant" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+  outputs = { nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      hostname = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.grant = import ./home.nix;
+          }
+        ];
       };
     };
+    # let
+    #   system = "x86_64-linux";
+    #   pkgs = nixpkgs.legacyPackages.${system};
+    # in {
+    #   nixosConfigurations = {
+    #     hostname = nixpkgs.lib.nixosSystem {
+    #       system = "x86_64-linux";
+    #       modules = [
+    #         ./configuration.nix
+    #         home-manager.nixosModules.home-manager
+    #         {
+    #           home-manager.useGlobalPkgs = true;
+    #           home-manager.useUserPackages = true;
+    #         }
+    #       ];
+    #     };
+    #   };
+
+    # homeConfigurations."grant" = home-manager.lib.homeManagerConfiguration {
+    #   inherit pkgs;
+
+    #   # Specify your home configuration modules here, for example,
+    #   # the path to your home.nix.
+    #   modules = [ ./home.nix ];
+
+    #   # Optionally use extraSpecialArgs
+    #   # to pass through arguments to home.nix
+    # };
+  };
 }
