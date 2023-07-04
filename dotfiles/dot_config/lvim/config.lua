b8dea21fa1a558dc1ee5910c54c9bfba51938161
Grant Ammons <gammons@gmail.com>
2023-07-04 06:43:09 -0400

    WIP on a better lvim setup

--- dotfiles/dot_config/lvim/config.lua
+++ dotfiles/dot_config/lvim/config.lua
@@ -8,34 +8,35 @@ lvim.plugins = {
   { "tpope/vim-fugitive" },
   { "tpope/vim-endwise" },
   { "ntpeters/vim-better-whitespace" },
-  {
-    "zbirenbaum/copilot-cmp",
-    event = "InsertEnter",
-    dependencies = { "zbirenbaum/copilot.lua" },
-    config = function()
-      vim.defer_fn(function()
-        local has_words_before = function()
-          if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-          return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-        end
+  { "github/copilot.vim" },
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   event = "InsertEnter",
+  --   dependencies = { "zbirenbaum/copilot.lua" },
+  --   config = function()
+  --     vim.defer_fn(function()
+  --       local has_words_before = function()
+  --         if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  --         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  --         return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+  --       end
 
-        require("copilot").setup()
-        local cmp = require("copilot_cmp")
-        cmp.setup({
-          mapping = {
-            ["<Tab>"] = vim.schedule_wrap(function(fallback)
-              if cmp.visible() and has_words_before() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-              else
-                fallback()
-              end
-            end),
-          },
-        })
-      end, 100)
-    end,
-  }
+  --       require("copilot").setup()
+  --       local cmp = require("copilot_cmp")
+  --       cmp.setup({
+  --         mapping = {
+  --           ["<Tab>"] = vim.schedule_wrap(function(fallback)
+  --             if cmp.visible() and has_words_before() then
+  --               cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+  --             else
+  --               fallback()
+  --             end
+  --           end),
+  --         },
+  --       })
+  --     end, 100)
+  --   end,
+  -- }
 }
 --------------------------------
 -- General config
@@ -49,6 +50,8 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.cmd("nnoremap Y Y")
 vim.cmd("let g:strip_whitespace_on_save=1")
+vim.cmd("let g:current_line_whitespace_disabled_soft=1")
+vim.cmd("let g:strip_whitespace_confirm=0")
 
 lvim.keys.normal_mode["<cr>"] = ":nohlsearch<cr>"
 lvim.keys.normal_mode["<C-p>"] = "<cmd>Telescope find_files<cr>"
